b5623fb00a741dc06b95970eb0d9da87a6d18563
Benny623 <bmilner88@gmail.com>
2022-06-21 15:28:24 -0700

    begin cards and add dateFormat.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends React.Component {
   constructor(props) {
@@ -31,25 +32,37 @@ class App extends React.Component {
     return (
       <div className="App">
         <header className="App-header">
-          <h2>git-commit-history</h2>
+          <h1>git-commit-history</h1>
         </header>
-        <h1>Commits:</h1>
-        <ul>
+        <h2 className='mt-3'>Commits:</h2>
+        <div className='container'>
           {
             items.map((item) => (
-              <a href={`${item.html_url}`} target='_blank' rel='noreferrer'>
-                <li className='card'>
-                  <img id='pfp' src={`${item.committer.avatar_url}`} alt='Committer profile' />
-                  username: { item.commit.author.name }
-                  commit message: { item.commit.message }
-                </li>
-              </a>
+              <div className='card' /* style='width: 18rem' */>
+                <div className='card-body'>
+                  <h5 className='card-title'>{item.commit.author.name}</h5>
+                  <h6 class='card-subtitle mb-2 text-muted'>{item.commit.committer.date}</h6>
+                  <p class='card-text'>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  <a href={`${item.html_url}`} target='_blank' rel='noreferrer' class='card-link'>Commit Link</a>
+                  <a href={`${item.committer.html_url}`} target='_blank' rel='noreferrer' class='card-link'>Committer's Profile</a>
+                </div>
+              </div>
             ))
           }
-        </ul>
+        </div>
       </div>
     );
   };
 };
 
 export default App;
+
+
+/* href={`${item.html_url}`} target='_blank' rel='noreferrer' */
+
+/* <li>
+                  Username: {item.commit.author.name}
+                </li>
+                <li>
+                  Commit Message: {item.commit.message}
+                </li> */
