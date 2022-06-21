1e9736e56730dec1046f4ca634a63172f9bc174e
Benny623 <bmilner88@gmail.com>
2022-06-21 13:53:52 -0700

    basic fetch req

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,7 +1,19 @@
 import React from 'react';
 
+let test;
+
+fetch(`https://api.github.com/repos/bmilner88/git-commit-history/commits`)
+        .then(response => {
+            if(response.ok) {
+                response.json().then(data => {
+                    test = data[0].commit.message
+                    console.log(data[0].commit.message)
+                })
+            }
+        })
+
 export default function Main() {
     return(
-        <div>Hello</div>
-    )
+        <div>{`${test}`}</div>
+    );
 };
